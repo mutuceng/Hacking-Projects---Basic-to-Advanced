f04ed4df609c911d139c8d4b745682a27211b483
mutuceng <umut.tanriverdiceng@gmail.com>
2024-04-18 13:19:10 +0300

    MAC ADDRESS CHANGER
    Mac address changer using python on virtual machine

---
+++ macAdressChanger/macChanger.py
@@ -0,0 +1,37 @@
+import subprocess
+import optparse
+import re
+def get_user_input():
+    parse_object = optparse.OptionParser()
+    parse_object.add_option("-i","--interface",dest="interface",help="interface to change")
+    parse_object.add_option("-m","--mac",dest="mac_address",help="new mac address")
+
+    return parse_object.parse_args()
+
+def macChanger(interface,mac_address):
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", mac_address])
+    subprocess.call(["ifconfig", interface, "up"])
+
+def changeControl(interface):
+    ifconfig = subprocess.check_output(["ifconfig",interface])
+
+    #using regex to find mac adresses on ifconfig
+    new_mac =  re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",str(ifconfig))
+
+    if new_mac:
+        return new_mac.group(0)
+    else:
+        return None
+
+
+print("Process started")
+
+(user_inputs,arguments) = get_user_input()             #Böyle daha clean
+macChanger(user_inputs.interface,user_inputs.mac_address)
+
+finalizedMac = changeControl(user_inputs.interface)
+if finalizedMac == user_inputs.mac_address:
+    print("SUCCES!")
+else:
+    print("ERROR!")
